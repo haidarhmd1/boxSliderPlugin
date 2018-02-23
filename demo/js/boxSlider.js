0a78959f2e0e6843611e06a232d64ec11cb67ebd
Haidar Hammoud <haidar.hmd1@gmail.com>
2018-02-23 14:29:20 +0200

    added styles

--- demo/js/boxSlider.js
+++ demo/js/boxSlider.js
@@ -198,3 +198,105 @@ jQuery.fn.upSlideStyle1 = function(backcolor,buttonText){
 
 
 //////////////// Modern Style ////////////////////
+
+
+
+
+
+//////////////// Modern Style 2 ////////////////////
+
+// left slider
+jQuery.fn.leftSlideStyle2 = function(backcolor, buttonText){
+
+    var element = $(this);
+
+    var boxHeight = $(element).height();
+    var boxWidth = $(element).width();
+
+    $(this).append("<div class='box-container-left-style2' style='height:"+ boxHeight +"px; width: "+ boxWidth +"px; background:"+ backcolor +"'><button class='btn-style style-2'>"+ buttonText +"</button><div>");
+
+    $(this).css({"position": "relative", "overflow": "hidden"});
+
+    $(this).mouseenter(function(e){
+        e.preventDefault();
+            $(".box-container-left-style2").css({"right": 0});
+    });
+
+    $(this).mouseleave(function(e){
+        e.preventDefault();
+            $(".box-container-left-style2").css({"right": 100 + "%"});
+    });
+};
+
+// down slider
+jQuery.fn.downSlideStyle2 = function(backcolor,buttonText){
+
+    var element = $(this);
+
+    var boxHeight = $(element).height();
+    var boxWidth = $(element).width();
+
+    $(this).append("<div class='box-container-down-style2' style='height:"+ boxHeight +"px; width: "+ boxWidth +"px; background:"+ backcolor +"'><button class='btn-style style-2'>"+ buttonText +"</button><div>");
+
+    $(this).css({"position": "relative", "overflow": "hidden"});
+
+    $(this).mouseenter(function(e){
+        e.preventDefault();
+            $(".box-container-down-style2").css({"bottom": 0});
+    });
+
+    $(this).mouseleave(function(e){
+        e.preventDefault();
+            $(".box-container-down-style2").css({"bottom": 100 + "%"});
+    });
+};
+
+// right slider
+jQuery.fn.rightSlideStyle2 = function(backcolor,buttonText){
+
+    var element = $(this);
+
+    var boxHeight = $(element).height();
+    var boxWidth = $(element).width();
+
+    $(this).css({"position": "relative", "overflow": "hidden"});
+
+    $(this).append("<div class='box-container-right-style2' style='height:"+ boxHeight +"px; width: "+ boxWidth +"px; background:"+ backcolor +"'><button class='btn-style style-2'>"+ buttonText +"</button><div>");
+
+    $(this).mouseenter(function(e){
+        e.preventDefault();
+            $(".box-container-right-style2").css({"left": 0});
+    });
+
+    $(this).mouseleave(function(e){
+        e.preventDefault();
+            $(".box-container-right-style2").css({"left": 100+"%"});
+    });
+};
+
+// bottom slider
+jQuery.fn.upSlideStyle2 = function(backcolor,buttonText){
+
+    var element = $(this);
+
+    var boxHeight = $(element).height();
+    var boxWidth = $(element).width();
+
+    $(this).css({"position": "relative", "overflow": "hidden"});
+
+    $(this).append("<div class='box-container-up-style2' style='height:"+ boxHeight +"px; width: "+ boxWidth +"px; background:"+ backcolor +"'><button class='btn-style style-2'>"+ buttonText +"</button><div>");
+
+    $(this).mouseenter(function(e){
+        e.preventDefault();
+            $(".box-container-up-style2").css({"top": 0});
+    });
+
+    $(this).mouseleave(function(e){
+        e.preventDefault();
+            $(".box-container-up-style2").css({"top": 100+"%"});
+    });
+};
+
+
+
+//////////////// Modern Style 2 ////////////////////
